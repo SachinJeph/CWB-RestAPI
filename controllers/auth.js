105d7a42ed2da97a7cf5147247b39f96c064a8bd
Nitin Goyal <nitingoyal.dev@gmail.com>
2016-11-09 16:02:50 +0000

    Bug Fix

--- controllers/auth.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var UserModel = require('../models/User');
-var jwt = require('jwt-simple');
-var moment = require('moment');
-
-module.exports.set = function(app){}
--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-var account = require('./account.js');
-var auth = require('./auth.js');
-
-module.exports.set = function(app){
-	account.set(app);
-	auth.set(app);
-}
